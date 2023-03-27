136337a077c01256c5596f439b9bd5e065e0175d
MD. SHA NEWAZ <70477038+mdshanewaz@users.noreply.github.com>
2023-03-27 23:23:00 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
 
+import math
+
+# quadratic equation and solution
+
+a = int(input())
+b = int(input())
+c = int(input())
+D = math.sqrt(b*b - 4*a*c)
+
+x1 = (-b+D)/2*a
+x2 = (-b-D)/2*a
+
+print(x1)
+print(x2)
+
+# swapping two variables without 3rd variable
+
+a = a-b
+b = a+b
+a = b-a
+
+print("a", a)
+print("b", b)
+
