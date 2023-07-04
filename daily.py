7fe4524d5902257b89c5a19a6b8b48e47ba3828a
Md Sha Newaz <sepnewaz9207@gmail.com>
2023-07-04 17:42:12 +0600

    ne

--- daily.py
+++ daily.py
@@ -1,1 +1,19 @@
-print("OK")
+user_are = "Mirpur"
+
+total_purchase_price = 100
+
+if user_are in ["Mirpur", "Motijil", "Monardip"]:
+
+    if total_purchase_price >= 600:
+        print("Shipping is free")
+    
+    elif total_purchase_price >= 300 and total_purchase_price < 600:
+        print("Shipping price is 80")
+    
+    else:
+        print("Shipping is 120")
+    
+# elif user_are in ["Mohakhali", "Uttora", "Gulshan"]:
+
+# else:
+#     print("Shipping is not available") 
