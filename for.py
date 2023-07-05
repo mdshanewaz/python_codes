16d3ba84bd9d84984bf84916c1cc16f2252329a0
Md Sha Newaz <sepnewaz9207@gmail.com>
2023-07-05 13:24:57 +0600

    wow

--- for.py
+++ for.py
@@ -2,3 +2,22 @@ my_list = ["apple", "orange", "Mango", "Lichi"]
 
 for f in my_list:
     print(f)
+
+print("")
+print("")
+
+for i in range(1, 10):
+    if i > 5:
+        break
+
+    print(i)
+
+print("")
+print("")
+
+
+for i in range(1, 10):
+    if i > 5 and i < 8:
+        continue
+
+    print(i)
