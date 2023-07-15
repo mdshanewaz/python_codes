87eeee8328c3ac1a730417b5e85a01691e5231a8
Md Sha Newaz <sepnewaz9207@gmail.com>
2023-07-15 09:34:51 +0600

    9:35am

--- dict.py
+++ dict.py
@@ -6,3 +6,33 @@ my_dict = {
 
 print(my_dict["skills"])
 print(my_dict.get("Age"))
+
+my_dict["name"] = "Test user"
+print(my_dict["name"])
+
+my_dict.update(
+    {
+        "name": "Shah Newaz",
+        "Skills": ("C++", "Python", "JavaScript"),
+    }
+)
+print(my_dict)
+
+my_dict["Age"] = 26
+print(my_dict)
+
+my_dict.update(
+    {
+        "Address" : "Feni",
+    }
+)
+print(my_dict)
+
+# del keyword
+del my_dict["Age"] 
+print(my_dict)
+
+# pop method
+my_dict.pop("Address")
+print(my_dict.pop("name"))
+print(my_dict)
