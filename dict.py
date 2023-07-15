c68c485ceda98297409736a72820b1be6ba726e3
Md Sha Newaz <sepnewaz9207@gmail.com>
2023-07-15 22:26:54 +0600

    10:26pm

--- dict.py
+++ dict.py
@@ -1,38 +1,61 @@
-my_dict = {
-    "name": "Shawon", 
-    "relagion": "Islam", 
-    "skills": ("Python", "JavaScript", "C++"),
+# my_dict = {
+#     "name": "Shawon", 
+#     "relagion": "Islam", 
+#     "skills": ("Python", "JavaScript", "C++"),
+# }
+
+# print(my_dict["skills"])
+# print(my_dict.get("Age"))
+
+# my_dict["name"] = "Test user"
+# print(my_dict["name"])
+
+# my_dict.update(
+#     {
+#         "name": "Shah Newaz",
+#         "Skills": ("C++", "Python", "JavaScript"),
+#     }
+# )
+# print(my_dict)
+
+# my_dict["Age"] = 26
+# print(my_dict)
+
+# my_dict.update(
+#     {
+#         "Address" : "Feni",
+#     }
+# )
+# print(my_dict)
+
+# # del keyword
+# del my_dict["Age"] 
+# print(my_dict)
+
+# # pop method
+# my_dict.pop("Address")
+# print(my_dict.pop("name"))
+# print(my_dict)
+
+#employee iterate
+employee = {
+    "Name" : "Niaz",
+    "Position" : "IT",
+    "Salary" : "15K",
 }
 
-print(my_dict["skills"])
-print(my_dict.get("Age"))
+print(employee.keys())
+print(employee.values())
+print(employee.items())
 
-my_dict["name"] = "Test user"
-print(my_dict["name"])
+for key in employee.keys():
+    print(key, employee[key])
 
-my_dict.update(
-    {
-        "name": "Shah Newaz",
-        "Skills": ("C++", "Python", "JavaScript"),
-    }
-)
-print(my_dict)
+for key, value in employee.items():
+    print(key, value)
 
-my_dict["Age"] = 26
-print(my_dict)
+employee_2 = employee
+print(employee_2)
 
-my_dict.update(
-    {
-        "Address" : "Feni",
-    }
-)
-print(my_dict)
-
-# del keyword
-del my_dict["Age"] 
-print(my_dict)
-
-# pop method
-my_dict.pop("Address")
-print(my_dict.pop("name"))
-print(my_dict)
+employee_3 = employee.copy()
+print(employee_3)
