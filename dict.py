628d84481b1b3229fd0ae03336a7e268fe1b3a7b
Md Sha Newaz <sepnewaz9207@gmail.com>
2023-07-15 08:33:13 +0600

    15th july 8:33

---
+++ dict.py
@@ -0,0 +1,8 @@
+my_dict = {
+    "name": "Shawon", 
+    "relagion": "Islam", 
+    "skills": ("Python", "JavaScript", "C++"),
+}
+
+print(my_dict["skills"])
+print(my_dict.get("Age"))
---
+++ set.py
@@ -0,0 +1,16 @@
+# set_1 = set()
+
+# print(type(set_1))
+
+# set = {"apple", "mango", "banana"}
+
+# print("mango" in set)
+
+user = {"Sazzad", "Shawon", "Rimi", "Sworna"}
+print(user)
+
+user.add("Test")
+print(user)
+
+user.remove("Shawon")
+print(user)
