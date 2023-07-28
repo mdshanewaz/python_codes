f17408e3400ed88e16ab984320e46b56fb18d22a
Md Sha Newaz <sawonnstu@gmail.com>
2023-07-28 22:55:35 +0600

    Hello

--- OPP/class_l.py
+++ OPP/class_l.py
@@ -4,4 +4,20 @@ class Person:
         self.age = age
         self.gender = gender
     
-persoon_1 = Person()
+    def introduce(self):
+        return "My name is {}".format(self.name)
+
+
+persoon_1 = Person("Shawon", 26, "Male")
+print(persoon_1.name)
+print(persoon_1.age)
+print(persoon_1.gender)
+print(persoon_1.introduce())
+print("\n")
+
+persoon_2 = Person("Rimi", 24, "Female")
+print(persoon_2.name)
+print(persoon_2.age)
+print(persoon_2.gender)
+print(persoon_2.introduce())
+print("\n")
