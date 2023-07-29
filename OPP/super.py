38ddf6658ddc7cacc20cf42c64e78ae9c492b014
Md Sha Newaz <sawonnstu@gmail.com>
2023-07-30 02:18:53 +0600

    Hello

--- OPP/super.py
+++ OPP/super.py
@@ -25,3 +25,31 @@ class D(C, B):
 
 obj_D = D()
 
+
+class User:
+
+    def __init__(self, name, total):
+        self.name = name
+        self.total = total
+
+    def get_shipping_cost(self):
+        if self.total > 1000:
+            return 40
+        return 60
+
+
+class Premium(User):
+
+    def get_discount(self):
+        if self.total > 2000:
+            return 100
+        return 20
+
+    def get_shipping_cost(self):
+        return 0
+
+preium_user = Premium("Shawon", 2000)
+normal_user = User("Test_user", 100)
+
+print(preium_user.get_shipping_cost())
+print(normal_user.get_shipping_cost())
