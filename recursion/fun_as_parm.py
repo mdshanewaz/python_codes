aa2c5505954bc883c69eb663aa8b25385a1ca415
Md Sha Newaz <sawonnstu@gmail.com>
2023-08-01 00:47:04 +0600

    hello

--- recursion/fun_as_parm.py
+++ recursion/fun_as_parm.py
@@ -1,2 +1,35 @@
+#function as a parameter in function
 def my_fun():
     print("Hello World!")
+
+def another_funct(func):
+    func()
+    print("This is from print function")
+
+another_funct(my_fun)
+
+
+#nested fun
+def greet(name):
+    def hello():
+        return "Hello my name is " + name
+    return  hello()
+VAR = greet("Shawon")
+print(greet)
+print(greet("Shawon"))
+
+#use decorator
+def great(fun):
+    def inner():
+        fun()
+        print("This is form inner of great")
+    return inner()
+
+@great
+def hellio():
+    print("This is hellio function")
+
+print(hellio())
+
+print(great)
+great(hellio)
