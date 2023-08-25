ea2fbf09be3fd2b3b7d0b7e685e81862399ccd43
Md Sha Newaz <sawonnstu@gmail.com>
2023-08-25 23:22:56 +0600

    New

--- problem_solving.py
+++ problem_solving.py
@@ -1,4 +1,5 @@
-n = int(input("Enter the value of n = "))
+
+n = int(input("n = "))
 
 number_list = list(range(1, n+1))
 
@@ -14,3 +15,41 @@ for i in number_list:
             print(i)
     else:    
         print(i)
+
+
+def reverse_words_order_and_swap_cases(sentence):
+    
+    print(sentence)
+
+    words = sentence.split(' ')[::-1]
+
+    swap_words = []
+
+    for i in words:
+        cont = i.swapcase()
+        swap_words.append(cont)
+
+    rev_sentence = ' '.join(swap_words)
+    print(rev_sentence)
+
+reverse_words_order_and_swap_cases("aWESOME is cODING")
+
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def area(self):
+        area = self.a * self.b
+        print("%.2f" % area)
+
+class Circle:
+    def __init__(self, a):
+        self.a = a
+    
+    def area(delf):
+        area = 3.1416 * a * a
+        print("%.2f" % area)
+
+
+        
