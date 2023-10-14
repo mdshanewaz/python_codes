328e27cbf79ed0132b8dc3a6440ea8912ba7b3ee
Md Sha Newaz <sawonnstu@gmail.com>
2023-10-14 16:39:57 +0600

    New

--- Add_Two_Numbers.py
+++ Add_Two_Numbers.py
@@ -41,3 +41,39 @@ def addTwoNumbers(l1:ListNode, l2:ListNode) -> ListNode:
         now.next = ListNode(carry)
         
     return head
+
+# Another way to solve it
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(0)
+
+        now = None
+
+        carry = 0
+
+        while l1 or l2:
+
+            sum = carry
+
+            if l1 is not None:
+                sum += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                sum += l2.val
+                l2 = l2.next
+
+            node = ListNode(sum % 10)
+
+            carry = sum // 10
+
+            if now is None:
+                now = head = node
+            else:
+                now.next = node
+                now = now.next
+        
+        if carry > 0:
+            now.next = ListNode(carry)
+        
+        return head
